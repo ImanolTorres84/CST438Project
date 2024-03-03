79df7b0cd85548ce1d7330a87ea7e32f9e3af807
CR3Swapper <chris@back.engineering>
2024-03-03 11:49:08 +0000

    register page looks good

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,11 @@
 <!-- Page body container -->
 <div class="container h-full mx-auto flex justify-center items-center">
 	<!-- Register Form -->
-	<form method="POST" class="w-1/2 bg-surface-600 p-10 space-y-4 rounded-md max-w-lg">
-		<CircleUserRound />
+	<form method="POST" class="w-2/3 bg-surface-600 p-10 space-y-4 rounded-md max-w-lg">
+		<center>
+			<!-- Centering -->
+			<CircleUserRound size={64} className="text-center"/>
+		</center>
 		<!-- Header -->
 		<div class="text-center font-bold text-2xl mb-4">Register User</div>
 		<p>Username</p>
