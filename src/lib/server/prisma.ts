c4e2f8484a95370ea124e33cb3fd0a370cdd2ccb
CR3Swapper <chris@back.engineering>
2024-03-18 22:38:28 -0700

    Untrack all files

--- src/lib/server/prisma.ts
+++
@@ -1,6 +0,0 @@
-/// The purpose of this file is to prevent a new database connection
-/// on every single hot reload (I.E everytime you fucking save)
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-export { prisma }
