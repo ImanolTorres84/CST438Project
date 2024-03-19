65d7bce05e6e839579c18e93e0d7fd0e9a49d86a
CR3Swapper <chris@back.engineering>
2024-03-19 19:31:47 +0000

    added authjs for google

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,32 +1,46 @@
 import { redirect } from '@sveltejs/kit';
 import { prisma } from "$lib/server/prisma";
+import { SvelteKitAuth } from "@auth/sveltekit";
+import Google from "@auth/sveltekit/providers/google";
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "$env/static/private";
+
+export const auth = SvelteKitAuth({
+    providers: [
+        Google({
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET
+        })
+    ]
+});
 
 // Unprotected routes go here!
-const unProtectedRoutes = ['/', '/login', '/register'];
+const unProtectedRoutes = ['/', '/login', '/register', '/auth/callback/google', '/auth'];
 
 export const handle = async ({ event, request, resolve }) => {
-    const sessionId = event.cookies.get('session');
-    if (!sessionId && !unProtectedRoutes.includes(event.url.pathname)) {
-        throw redirect(303, '/');
-    }
-
-    const userInfo = await prisma.user
-    .findFirst({
-        where: { session: sessionId },
-    });
-    
-    if (userInfo) {
-        event.locals.user = {
-            id: userInfo.id,
-            isAuthenticated: true,
-            email: userInfo.email,
-            username: userInfo.username,
-        };
+    if (unProtectedRoutes.includes(event.url.pathname)) {
+        return auth.handle({ event, request, resolve });
     } else {
-        if (!unProtectedRoutes.includes(event.url.pathname)) {
+        const sessionId = event.cookies.get('session');
+        if (!sessionId) {
             throw redirect(303, '/');
         }
-    }
 
-    return resolve(event);
+        const userInfo = await prisma.user
+            .findFirst({
+                where: { session: sessionId },
+            });
+
+        if (userInfo) {
+            event.locals.user = {
+                id: userInfo.id,
+                isAuthenticated: true,
+                email: userInfo.email,
+                username: userInfo.username,
+            };
+        } else {
+            if (!unProtectedRoutes.includes(event.url.pathname)) {
+                throw redirect(303, '/');
+            }
+        }
+    }
 };
