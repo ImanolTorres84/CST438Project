42c67c467aa6a0190f76a711abd98a0d21b741e7
Imanol Torres Diaz <itorresdiaz@csumb.edu>
2024-03-21 10:16:09 +0000

    created pet profile page/ working on connecting to database

---
+++ src/routes/petprofile/+page.svelte
@@ -0,0 +1,45 @@
+<style>
+ .all {
+    background-color: white;
+    align-items: center;
+    text-align: center;
+    color: black;
+ }
+ .right{
+    align-items: right;
+ }
+ .left{
+    align-items: left;
+ }
+</style>
+
+<div>
+   <div>
+    <h1>Your Pet Information</h1>
+   </div>
+
+   <br>
+   <br>
+
+   <div class="all">
+      <div class="left">
+         <h2>Pet Image:</h2>
+      </div>
+      <div class="right">
+         <p>Insert Image Of Pet From Database Here</p>
+      </div>
+   <br>
+
+   <div>
+    <h2 class="left">User Profile:</h2>
+    <p>Link to profile here from database</p>
+   </div>
+
+   <br>
+
+   <div>
+    <h2>Your Pet Posts:</h2>
+    <p>Insert Posts from database here</p>
+   </div>
+    </div>
+</div>
