38e0783334eb5a5a36d82981d647cff19c78bc76
Imanol Torres Diaz <itorresdiaz@csumb.edu>
2024-03-31 05:05:46 +0000

    Set up connecttion to schema on profile page

--- src/routes/userprofile/+page.svelte
+++ src/routes/userprofile/+page.svelte
@@ -1,52 +1,32 @@
-<style>
- .all {
-    background-color: white;
-    align-items: center;
-    text-align: center;
-    color: black;
- }
- .right{
-    align-items: right;
- }
- .left{
-    align-items: left;
- }
-</style>
+
+<script>
+    //This will conect to the prisma
+    import {onMount} from 'svelte';
+    import {PrismaClient} from '@prisma/client';
+	import { profile } from 'console';
+    const prisma = new PrismaClient();
+
+</script>
+
+<h1>Profile Information</h1>
+
+<br>
+<br>
 
 <div>
-   <div>
-    <h1>Your Profile Information</h1>
-   </div>
+    <h2>Basic Information</h2>
 
-   <br>
-   <br>
+    <p>User Name: </p>
+    <p>Display Name: </p>
+    <p>Profile Picture: </p>
+    <p>Location: </p>
+    <p>Preferred Language: </p>
+</div>
 
-   <div class="all">
-    <div>
-   <div class="left">
-    <h2>Profile Image:</h2>
-   </div>
-   <div class="right">
-    <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.rawpixel.com%2Fimage_png_800%2FcHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTAxL3JtNjA5LXNvbGlkaWNvbi13LTAwMi1wLnBuZw.png&tbnid=FwT2CRstlsNt4M&vet=12ahUKEwjJmsHvi4WFAxWyIEQIHaCBCeMQMygBegQIARB1..i&imgrefurl=https%3A%2F%2Fwww.rawpixel.com%2Fsearch%2Fprofile%2520icon&docid=-QpGQOw4ca8j3M&w=800&h=800&q=profile%20icon%20images&ved=2ahUKEwjJmsHvi4WFAxWyIEQIHaCBCeMQMygBegQIARB1" alt="Icon">
-   </div>
-    </div>
+<div>
+    <h2>Friends List</h2>
+</div>
 
-   <br>
-
-   <div>
-    <h2 class="left">Friends List:</h2>
-   </div>
-
-   <br>
-
-   <div>
-    <h2>Pet Profiles:</h2>
-   </div>
-
-   <br>
-
-   <div>
-    <h2>Your Posts:</h2>
-   </div>
-    </div>
+<div>
+    <h2>Your Posts</h2>
 </div>
